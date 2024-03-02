97b1c3f8586e223be9b0a05f3c345a3cd165abcb
AanAkbar <144786360+AanAkbar@users.noreply.github.com>
2024-03-02 18:03:24 +0700

    Update visualization.py

--- visualization.py
+++ visualization.py
@@ -6,3 +6,10 @@ uploaded_file = st.file_uploader('Choose a CSV file')
 if uploaded_file:
     df = pd.read_csv(uploaded_file)
     st.dataframe(df)
+
+
+
+values = st.slider(
+    label='Select a range of values',
+    min_value=0, max_value=100, value=(0, 100))
+st.write('Values:', values)
