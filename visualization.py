a2b1f5179684597bbd444985a06edcabf24a8c11
AanAkbar <144786360+AanAkbar@users.noreply.github.com>
2024-03-02 18:12:33 +0700

    Update visualization.py

--- visualization.py
+++ visualization.py
@@ -3,13 +3,6 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
 
- 
-uploaded_file = st.file_uploader('Choose a CSV file')
- 
-if uploaded_file:
-    df = pd.read_csv(uploaded_file)
-    st.dataframe(df)
-
 
 
 
