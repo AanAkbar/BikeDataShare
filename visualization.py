c94cfe5fee91020aedc2bd56297507ff845d101c
AanAkbar <144786360+AanAkbar@users.noreply.github.com>
2024-03-02 18:08:44 +0700

    Update visualization.py

--- visualization.py
+++ visualization.py
@@ -14,12 +14,13 @@ import matplotlib.pyplot as plt
 import numpy as np
  
 with st.container():
-    st.write("Inside the container")
+    st.write("Month")
     
     x = np.random.normal(15, 5, 250)
  
     fig, ax = plt.subplots()
-    ax.hist(x=x, bins=15)
+    ax.hist(x="mnth", bins="cnt)
     st.pyplot(fig) 
  
-st.write("Outside the container")
+st.write("Count of Rentals")
+
